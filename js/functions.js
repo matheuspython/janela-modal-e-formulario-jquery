fa1998107370501d3348eedfad18788ee8a49d23
matheus souza <ruivorobado@gmail.com>
2020-10-27 20:04:16 -0300

    programada a função para ao clicar fora do formulario sair dele

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,20 @@
 $(function(){
     abrirjanela();
-
+    verificarCliqueFechar()
     function abrirjanela(){
-        $('.btn').click(function(){
+        $('.btn').click(function(e){
+            e.stopPropagation();//impede que o evento se propague e va pro elemento pai usado pro fadeOut funcionar direito
             $('.bg').fadeIn();//ao clicar no btn vai acontecer uma animacao do bg aparecendo
         });
     }
+    function verificarCliqueFechar(){
+        var el = $('body');
+        el.click(function(){
+            $('.bg').fadeOut();
+            $('.form').click(function(e){
+                e.stopPropagation();//mesma ideia do stopPropagation anterior
+            })
+        })
+    }
+
 });
