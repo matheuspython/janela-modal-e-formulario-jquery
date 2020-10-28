c7c38c1cbe21f85b9f1da7ad325b008cbdcbefa6
matheus souza <ruivorobado@gmail.com>
2020-10-28 18:06:04 -0300

    funcoes para verificar se os campos do formulario estao validos inicializada

--- js/functions.js
+++ js/functions.js
@@ -8,13 +8,45 @@ $(function(){
         });
     }
     function verificarCliqueFechar(){
-        var el = $('body');
+        var el = $('body,.closeBtn');
         el.click(function(){
             $('.bg').fadeOut();
+        })
             $('.form').click(function(e){
                 e.stopPropagation();//mesma ideia do stopPropagation anterior
             })
-        })
+     
     }
+    $('form#form1').submit(function(e){
+        e.preventDefault()
+        var nome = $('input[name=nome]').val()
+        var telefone = $('input[name=telefone]').val()
+        var email = $('input[name=email]').val()
+        
+        var amount = nome.split(' ').length//contando a quantidade de espaços e seus valores
+        var amounts = nome.split(' ')
+        if(amount >= 2){//o split ta dividindo a var nome em arrays e o leng ta contando quantos elementos tem nesse array e se tiver mais de 2 entra no if
+            for(i=0;i<amount;i++){
+                if(amounts[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
+                 
+                }else{
+                    aplicarCamploInvalido($('input[name=nome]'))
+                    return false
+                }
+            }
 
+        }else{
+            aplicarCamploInvalido($('input[name=nome]'))
+            return false
+        }
+
+
+   
+    })
+function aplicarCamploInvalido(el){
+    el.css('border','2px solid red')
+                    
+    el.data('invalido','targetTouches')
+    el.val('campo invalido1')      
+}
 });
